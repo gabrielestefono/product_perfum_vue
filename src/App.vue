64ad56f193718cf16ccb5ab866256f66e07f09b8
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-29 21:34:39 -0300

    Projeto Finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Product></Product>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue';
 
-export default {
+export default{
   name: 'App',
   components: {
-    HelloWorld
+    Product
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url(reset.css);
+@import url('https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+/* Estilizando todo o body*/ 
+body{
+    font-family: 'Montserrat', sans-serif;
+    background-color: hsl(30, 38%, 92%);
 }
+@media screen and (min-width: 376px){
+    /* Estilizando o body*/
+    body{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="container">
+        <div class="imgContainer"></div>
+        <div class="bodyConteiner">
+            <div class="textContainer">
+                <h1 class="textContainer__title">perfume</h1>
+                <h2 class="textContainer__subtitle">Gabrielle Essence Eau De Parfum</h2>
+                <p class="textContainer__paragraph">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                <div class="textContainer__price">
+                    <p class="textContainer__price--actual">$149.99</p>
+                    <p class="textContainer__price--before">$169.99</p>
+                </div>
+                <button class="textContainer__chart"><i class="fa-solid fa-cart-shopping"></i> Add to Cart</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Product'
+}
+</script>
+
+<style>
+    .container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+/* Estilizando imagem no mostrador*/ 
+.imgContainer{
+    width: 21.4375rem;
+    height: 15rem;
+    background-repeat: none;
+    background-size: cover;
+    border-radius: 10px 10px 0 0;
+    margin-top: 1.75rem;
+    background-image: url(image-product-mobile.jpg);
+}
+
+/*Estilizando a parte de baixo*/
+.bodyConteiner{
+    background-color: white;
+    width: 21.4375rem;
+    display: flex;
+    justify-content: center;
+    border-radius: 0 0 10px 10px;
+}
+
+.textContainer{
+    width: 18rem;
+}
+
+.textContainer__title{
+    margin-top: 1.75rem;
+    letter-spacing: 5px;
+    text-transform: uppercase;
+    font-size: 12px;
+    color: hsl(228, 12%, 48%);
+}
+
+.textContainer__subtitle{
+    margin-top: 1rem;
+    font-family: 'Fraunces', serif;
+    font-weight: 700;
+    font-size: 2rem;
+    color: hsl(212, 21%, 14%);
+}
+
+.textContainer__paragraph{
+    margin-top: 1rem;
+    font-size: 14px;
+    line-height: 24px;
+    color: hsl(228, 12%, 48%);
+}
+
+.textContainer__price{
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+}
+
+.textContainer__price--actual{
+    font-family: 'Fraunces', serif;
+    font-size: 2rem;
+    font-weight: 700;
+    color: hsl(158, 36%, 37%);
+}
+
+.textContainer__price--before{
+    margin-left: 1rem;
+    font-size: 14px;
+    text-decoration:line-through;
+    color: hsl(228, 12%, 48%);
+}
+
+.textContainer__chart{
+    margin-top: 1rem;
+    margin-bottom: 1.5rem;
+    height: 3rem;
+    border-radius: 5px;
+    width: 18rem;
+    border: none;
+    background-color: hsl(158, 36%, 37%);
+    color: white;
+    font-weight: 700;
+}
+
+@media screen and (min-width: 376px){
+    /* Estilizando o body*/
+    .container{
+        flex-direction: row;
+        max-width: 600px;
+        max-height: 450px;
+    }
+
+    /* Estilizando imagem no mostrador*/ 
+    .imgContainer{
+        width: 300px;
+        height: 450px;
+        background-image: url(image-product-desktop.jpg);
+        border-radius: 10px 0 0 10px;
+        margin-top: 0;
+    }
+
+    .bodyConteiner{
+        width: 300px;
+        height: 450px;
+        border-radius: 0 10px 10px 0;
+    }
+
+    .textContainer{
+        margin: 0 auto;
+        width: 15rem;
+    }
+
+    .textContainer__subtitle{
+        margin-top: 1.25rem;
+    }
+    
+    .textContainer__paragraph{
+        margin-top: 1.25rem;
+    }
+    
+    .textContainer__price{
+        margin-top: 1.25rem;
+    }
+    
+    .textContainer__price--actual{
+        margin-top: 1.25rem;
+    }
+    
+    .textContainer__price--before{
+        margin-top: 1.25rem;
+    }
+    
+    .textContainer__chart{
+        margin-top: 1.25rem;
+        width: 15rem;
+        cursor: pointer;
+    }
+
+    .textContainer__chart:hover{
+        background-color: rgb(26,64,49);
+    }
+
+
+}
+</style>
